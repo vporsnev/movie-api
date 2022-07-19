72e143ede60d0a72089292aafd8ee0832aa63f96
vporsnev <v.porshnev@gmail.com>
2022-07-19 22:20:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,17 @@ let movies = [
     imageURL: 'https://i.ebayimg.com/images/g/NDEAAOSwKfVXLviD/s-l500.jpg',
     featured: false
   },
+  {
+    title: 'Django Unchained',
+    discription: 'With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.',
+    genre: 'Western',
+    director: {
+      name: 'Quentin Tarantino',
+      bio: 'Quentin Jerome Tarantino was born in Knoxville, Tennessee. His father, Tony Tarantino, is an Italian-American actor and musician from New York, and his mother, Connie (McHugh), is a nurse from Tennessee. Quentin moved with his mother to Torrance, California, when he was four years old. In January of 1992, first-time writer-director Tarantino\'s Reservoir Dogs (1992) appeared at the Sundance Film Festival. The film garnered critical acclaim and the director became a legend immediately. Two years later, he followed up Dogs success with Pulp Fiction (1994) which premiered at the Cannes film festival, winning the coveted Palme D Or Award. At the 1995 Academy Awards, it was nominated for the best picture, best director and best original screenplay. Tarantino and writing partner Roger Avary came away with the award only for best original screenplay. In 1995, Tarantino directed one fourth of the anthology Four Rooms (1995) with friends and fellow auteurs Alexandre Rockwell, Robert Rodriguez and Allison Anders. The film opened December 25 in the United States to very weak reviews. Tarantino\'s next film was From Dusk Till Dawn (1996), a vampire/crime story which he wrote and co-starred with George Clooney. The film did fairly well theatrically. Since then, Tarantino has helmed several critically and financially successful films, including Jackie Brown (1997), Kill Bill: Vol. 1 (2003), Kill Bill: Vol. 2 (2004), Inglourious Basterds (2009), Django Unchained (2012) and The Hateful Eight (2015).',
+    },
+    imageURL: 'https://static.posters.cz/image/750/bilderdjango-unchained-life-liberty-and-the-pursuit-of-vengeance-i23683.jpg',
+    featured: false
+  }
 ];
 
 // GET requests
