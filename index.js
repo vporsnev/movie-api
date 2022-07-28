65570dba9f716f471538531114ed02c21d2d11cc
vporsnev <v.porshnev@gmail.com>
2022-07-28 12:37:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ //support parsing of application/x-www-form-urle
 //   useUnifiedTopology: true
 // });
 
-mongoose.connect('process.env.CONNECTION_URI', {
+mongoose.connect( process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
